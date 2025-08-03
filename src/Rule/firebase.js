180cb89c5c59777b936dd5abf6338b3f27a9c54d
Nikhil-Mishra12 <nm280477@gmail.com>
2025-08-03 13:56:56 +0530

    fixed firebase config and imports

--- src/Rule/firebase.js
+++ src/Rule/firebase.js
@@ -1,13 +1,13 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth/cordova";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: "netflixgpt-622f7.firebaseapp.com",
   projectId: "netflixgpt-622f7",
-  storageBucket: "netflixgpt-622f7.firebasestorage.app",
+  storageBucket: "netflixgpt-622f7.firebasestorage.appspot.com",
   messagingSenderId: "375260651925",
   appId: "1:375260651925:web:6bcc2a401bec702c98169c",
   measurementId: "G-41X654FCTG"
