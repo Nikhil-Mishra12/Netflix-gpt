eadfc5d77477eef2395c91eb8152caafc7c6b2d9
Nikhil-Mishra12 <nm280477@gmail.com>
2025-08-03 13:39:41 +0530

    .env file

--- src/Rule/firebase.js
+++ src/Rule/firebase.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth/cordova";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAH7atIyWHAU73US2IxOt_k_uWao-CZ_7M",
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: "netflixgpt-622f7.firebaseapp.com",
   projectId: "netflixgpt-622f7",
   storageBucket: "netflixgpt-622f7.firebasestorage.app",
@@ -17,7 +13,7 @@ const firebaseConfig = {
   measurementId: "G-41X654FCTG"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth=getAuth()
